0b59f0074004cbf6054d58b9362bb937d7ee2f33
Josh Obatay <joshobatay2005@gmail.com>
2025-06-23 19:05:27 +0800

    added the remaining lines of codes essential for 'object_handler.py'

--- object_handler.py
+++ object_handler.py
@@ -70,3 +70,15 @@ class ObjectHandler:
             pg.display.flip()
             pg.time.delay(1500)
             self.game.new_game()
+
+    def update(self):
+        self.npc_positions = {npc.map_pos for npc in self.npc_list if npc.alive}
+        [sprite.update() for sprite in self.sprite_list]
+        [npc.update() for npc in self.npc_list]
+        self.check_win()
+
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
+
+    def add_sprite(self, sprite):
+        self.sprite_list.append(sprite)
