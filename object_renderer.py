ff1518cf546ce218720715f5dcecc21dc639f7c1
Josh Obatay <joshobatay2005@gmail.com>
2025-06-23 19:07:15 +0800

    added the essential lines of codes for 'object_renderer.py'

---
+++ object_renderer.py
@@ -0,0 +1,23 @@
+import pygame as pg
+from settings import *
+
+
+class ObjectRenderer:
+    def __init__(self, game):
+        self.game = game
+        self.screen = game.screen
+        self.wall_textures = self.load_wall_textures()
+        self.sky_image = self.get_texture('resources/textures/sky.png', (WIDTH, HALF_HEIGHT))
+        self.sky_offset = 0
+        self.blood_screen = self.get_texture('resources/textures/blood_screen.png', RES)
+        self.digit_size = 90
+        self.digit_images = [self.get_texture(f'resources/textures/digits/{i}.png', [self.digit_size] * 2)
+                             for i in range(11)]
+        self.digits = dict(zip(map(str, range(11)), self.digit_images))
+        self.game_over_image = self.get_texture('resources/textures/game_over.png', RES)
+        self.win_image = self.get_texture('resources/textures/win.png', RES)
+
+    def draw(self):
+        self.draw_background()
+        self.render_game_objects()
+        self.draw_player_health()
